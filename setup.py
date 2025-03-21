ea4393b399c0f1f05086cfb16a65377d2b7859d6
Mastergive <86574125+Mastergive@users.noreply.github.com>
2025-03-21 11:37:05 +0000

    fixing bugs [only works in windows!!]

--- setup.py
+++ setup.py
@@ -6,24 +6,24 @@ startbatch = """@ECHO OFF
 ECHO Starting NGINX Server..
 ECHO Running in http://localhost:8080
 ECHO To stop the server, please execute stop.bat
-start C:\RTMPServer\nginx.exe
+start C:\\RTMPServer\\nginx.exe
 """
 stopbatch = """@ECHO OFF
 
 ECHO Stopping NGINX Server..
-start C:\RTMPServer\nginx.exe -s stop
+start C:\\RTMPServer\\nginx.exe -s stop
 """
 
-user = os.envrion.get()
-startname = "C:\Users\" + user + "\Desktop\start.bat"
-stopname = "C:\Users\" + user + "\Desktop\stop.bat"
+user = os.environ.get("USERNAME")
+startname = "C:\\Users\\" + user + "\\Desktop\\start.bat"
+stopname = "C:\\Users\\" + user + "\\Desktop\\stop.bat"
 
 if os.name == "nt":
-  DEFAULTDIR = "C:\RTMPServer\"
+  DEFAULTDIR = "C:\\RTMPServer\\"
 elif os.name == "posix":
   DEFAULTDIR = "/home/" + user + "/RTMPServer/"
   print("currently, i didnt make a script for linux. i will do it soon!")
-  break
+  
 
 def createbatch():
   with open(startname, "w") as file:
@@ -34,9 +34,9 @@ def createbatch():
 def downloadwin():
   print("Downloading nginx-rtmp-win32...")
   try:
-    os.system("cd C:\")
+    os.system("cd C:\\")
     os.system("mkdir RTMPServer")
-    os.system("git clone https://github.com/illuspas/nginx-rtmp-win32.git C:\RTMPServer")
+    os.system("git clone https://github.com/illuspas/nginx-rtmp-win32.git C:\\RTMPServer")
   except error:
     print("error: " + error)
     print("Tip: Make sure you have git installed! Download it here https://git-scm.com/downloads")
@@ -45,9 +45,9 @@ def downloadwin():
   print("Do you want a batch in your desktop to run the Nginx server? [yes/no]")
   i = input("--> ")
   if i == "no":
-    break
+    print("ok")
   elif i == "yes":
     print("Creating the batch file")
     createbatch()
-    break
-
+   
+downloadwin()
